263cba214126ec3ca13d5f0f44fdc1a55edcc983
baimenovbn <baibn@mail.ru>
2020-10-22 10:37:18 +0600

    feat: add reverse fn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (n === undefined) return;
+
+  return parseInt(n
+                    .toString()
+                    .split('')
+                    .reverse()
+                    .join('')
+                  );
 }
